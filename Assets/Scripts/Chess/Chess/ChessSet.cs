85052f416a6eb4b716c56ec708f6bd7f553e9848
sweetGrace <news864985444@163.com>
2022-12-03 23:37:36 +0800

    Complete Selector.

--- Assets/Scripts/Chess/Chess/ChessSet.cs
+++ Assets/Scripts/Chess/Chess/ChessSet.cs
@@ -1,8 +1,8 @@
 using System.Collections.Generic;
 
 public class ChessSet : IRoundable {
-    private List<Chess> _chesses = new List<Chess>();
-    public IMutiSelector selector { get; private set; }
+    public List<Chess> chesses = new List<Chess>();
+    public IMultiSelector selector { get; private set; }
 
     public void OnRoundBegin(RoundManager round) {}
 
---
+++ Assets/Scripts/Chess/Selector/MultiSelector/LineSelector.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LineSelector : IMultiSelector {
+    float halfSqrt2 = Mathf.Sqrt(2) / 2;
+    private float _DistofPtoL(Vector2 lineStart, Vector2 lineEnd, Vector2 point) {
+        if (lineStart.x == lineEnd.x) {
+            return Mathf.Abs(point.y-lineStart.y);
+        }else if (lineStart.y == lineEnd.y) {
+            return Mathf.Abs(point.x-lineStart.x);
+        }else {
+            float slope = (lineEnd.y - lineStart.y) / (lineEnd.x - lineStart.x);
+            float intercept = lineEnd.y - slope * lineEnd.x;
+            return Mathf.Abs(slope * point.x - point.y) / Mathf.Sqrt(slope * slope +1);
+        }
+        
+    }
+    public List<Vector2> Select(ChessSet set) {
+        
+        List<Vector2> allpoint = new List<Vector2> {};
+        float UpperY = set.chesses[0].position.y, DownY = set.chesses[0].position.y, LeftX = set.chesses[0].position.x, RightX = set.chesses[0].position.x;
+        for (int i = 0; i <= set.chesses.Count(); i++) { 
+            if (set.chesses[i].position.x < LeftX) { LeftX=set.chesses[i].position.x;}
+            if (set.chesses[i].position.x > RightX) { RightX=set.chesses[i].position.x;}
+            if (set.chesses[i].position.y > UpperY) { UpperY=set.chesses[i].position.y;}
+            if (set.chesses[i].position.y < DownY) { DownY=set.chesses[i].position.y;}
+
+        }
+        for (int axisX = (int) (LeftX + 1E-7); axisX <= (int) (RightX + 1E-7); axisX++) {
+            for (int axisY = (int) (DownY + 1E-7); axisY <= (int) (UpperY + 1E-7); axisY++) {
+                Vector2 targetPoint=new Vector2(axisX, axisY);
+                for (int i = 0, j = set.chesses.Count(); i <= set.chesses.Count(); j=i, i++) {
+                    if (_DistofPtoL(set.chesses[j].position, set.chesses[i].position, targetPoint) < halfSqrt2) {
+                        allpoint.Add(targetPoint);
+                    }
+                }  
+            }
+        }
+        return allpoint;
+    }
+}
+        
