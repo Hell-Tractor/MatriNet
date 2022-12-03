3baabb285ec0f65da5de8de6ec5a1bbabb07c516
Hell_Tractor <1104840448@qq.com>
2022-12-03 20:02:27 +0800

    Add single selector

---
+++ Assets/Scripts/Chess/Selector/SingleSelector/UpSelector.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class UpSelector : ISingleSelector {
+    public List<Chess> Select(Chess chess, List<Chess> allChess) {
+        return allChess.Where(c => c.position.x == chess.position.x && c.position.y > chess.position.y).ToList();
+    }
+}
