8df3cfc0e035fa0fa3f90ce64b1576651c6a9e7f
Hell_Tractor <1104840448@qq.com>
2022-12-04 21:46:51 +0800

    Modify shop

--- Assets/Scripts/UI/ShopUI.cs
+++ Assets/Scripts/UI/ShopUI.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ShopUI : MonoBehaviour,IPointerClickHandler
+public class ShopUI : MonoBehaviour, IPointerClickHandler
 {
     public GameObject manager;
     public DrawCards chipCards;
@@ -12,55 +12,65 @@ public class ShopUI : MonoBehaviour,IPointerClickHandler
     public DrawCards bombCards;
     public Text coinNumber;
 
+    public GameObject shopWindow;
+
+    void Start()
+    {
+        coinNumber.text = manager.GetComponent<PlayerInfo>().Money.ToString();
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
-        if(eventData.button == PointerEventData.InputButton.Right)
+        if (eventData.button == PointerEventData.InputButton.Right)
         {
-           return;
+            return;
         }
-        
-            if (name == "shopchip")
-            {
-                Debug.Log("chip");
-                if(manager.GetComponent<PlayerInfo>().Money >= 2)
-                {
-                    manager.GetComponent<PlayerInfo>().Money -= 2;
-                    chipCards.count++;
-                    coinNumber.text=manager.GetComponent<PlayerInfo>().Money.ToString();
-                    chipCards.countText.text = chipCards.count.ToString();
-                }
 
-            }
-            else if (name == "shopmirror")
+        if (name == "shopchip")
+        {
+            Debug.Log("chip");
+            if (manager.GetComponent<PlayerInfo>().Money >= 2)
             {
-                Debug.Log("mirror");
-                if(manager.GetComponent<PlayerInfo>().Money >= 3)
-                {
-                    manager.GetComponent<PlayerInfo>().Money -= 3;
-                    mirrorCards.count++;
-                    coinNumber.text=manager.GetComponent<PlayerInfo>().Money.ToString();
-                    mirrorCards.countText.text = chipCards.count.ToString();
-                }
-                
-            }
-            else if (name == "shopbomb")
-            {
-                Debug.Log("bomb");
-                if(manager.GetComponent<PlayerInfo>().Money >= 5)
-                {
-                    manager.GetComponent<PlayerInfo>().Money -= 5;
-                    bombCards.count++;
-                    coinNumber.text=manager.GetComponent<PlayerInfo>().Money.ToString();
-                    bombCards.countText.text = bombCards.count.ToString();
-                }
-
+                manager.GetComponent<PlayerInfo>().Money -= 2;
+                chipCards.count++;
+                coinNumber.text = manager.GetComponent<PlayerInfo>().Money.ToString();
+                chipCards.countText.text = chipCards.count.ToString();
             }
 
+        }
+        else if (name == "shopmirror")
+        {
+            Debug.Log("mirror");
+            if (manager.GetComponent<PlayerInfo>().Money >= 3)
+            {
+                manager.GetComponent<PlayerInfo>().Money -= 3;
+                mirrorCards.count++;
+                coinNumber.text = manager.GetComponent<PlayerInfo>().Money.ToString();
+                mirrorCards.countText.text = mirrorCards.count.ToString();
+            }
 
-        
+        }
+        else if (name == "shopbomb")
+        {
+            Debug.Log("bomb");
+            if (manager.GetComponent<PlayerInfo>().Money >= 5)
+            {
+                manager.GetComponent<PlayerInfo>().Money -= 5;
+                bombCards.count++;
+                coinNumber.text = manager.GetComponent<PlayerInfo>().Money.ToString();
+                bombCards.countText.text = bombCards.count.ToString();
+            }
+
+        }
 
 
 
+
+
+
+    }
+    public void CloseShop()
+    {
+        shopWindow.SetActive(false);
     }
 
 }
